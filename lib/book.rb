c977033edb280a3ff4c5fa58295ddd6292e1ef4d
J <jennifer.singleton@gmail.com>
2019-01-20 21:04:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -9,27 +9,27 @@ class Book
   # def title
   #   @title
   # end
-  # 
+  #
   # def author=(author)
   #   @author = author
   # end
-  # 
+  #
   # def author
   #   @author
   # end
-  # 
+  #
   # def page_count=(page)
   #   @page_count = page
   # end
-  # 
+  #
   # def page_count
   #   @page_count
   # end
-  # 
+  #
   # def genre=(genre)
   #   @genre = genre
   # end
-  # 
+  #
   # def genre
   #   @genre
   # end
